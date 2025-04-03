b81d5f767bb21356f5367e206a8d3d6d65f2d0a1
Christian Sorensen <ctsorensen2019@gmail.com>
2025-04-03 10:38:11 -0600

    Fixed categoryfilter

--- frontend/src/components/CategoryFilter.tsx
+++ frontend/src/components/CategoryFilter.tsx
@@ -14,7 +14,7 @@ function CategoryFilter({
     const fetchCategories = async () => {
       try {
         const response = await fetch(
-          'https://localhost:5000/Book/GetBookTypes'
+          'https://mission13-sorensen-backend-b6fchrh0gmcqhxak.eastus-01.azurewebsites.net/Book/GetBookTypes'
         );
         const data = await response.json();
         console.log('Fetched categories:', data);
